37ba02af249871fd7858c8994963af92038fa396
KevinTss <kevin.tassi@gmail.com>
2020-09-19 10:09:59 +0200

    Config eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended', 'plugin:prettier/recommended'
+  ],
+  rules: {
+    "vue/component-definition-name-casing": ["warn", "kebab-case"]
+  }
+}
