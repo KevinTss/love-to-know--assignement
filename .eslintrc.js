a3e6c9bfd8046cff5f09fd48f2a7b728588dd804
KevinTss <kevin.tassi@gmail.com>
2020-09-19 15:58:52 +0200

    config: improve linting experience

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,7 @@ module.exports = {
   extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
   rules: {
     'vue/component-definition-name-casing': ['warn', 'kebab-case'],
-    'vue/max-attributes-per-line': [
-      'warn',
-      {
-        singleline: 1,
-        multiline: {
-          max: 1,
-          allowFirstLine: false,
-        },
-      },
-    ],
+    'vue/max-attributes-per-line': "off",
+    'vue/html-self-closing': "off"
   },
 }
