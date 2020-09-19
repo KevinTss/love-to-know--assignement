31b6ae49b4b29cf9d2184199efd0fe2a18a78c0a
KevinTss <kevin.tassi@gmail.com>
2020-09-19 11:42:09 +0200

    Config eslint for max attributes per line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  extends: [
-    'plugin:vue/vue3-recommended', 'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
   rules: {
-    "vue/component-definition-name-casing": ["warn", "kebab-case"]
-  }
+    'vue/component-definition-name-casing': ['warn', 'kebab-case'],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+  },
 }
