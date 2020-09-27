0f3acebed55b3a7df9fc64e653f4d77108b467d8
KevinTss <kevin.tassi@gmail.com>
2020-09-27 18:34:29 +0200

    Create SASS variables & mixins

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: './',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/style/_variables.scss";
+          @import "@/style/_mixins.scss";
+        `,
+      },
+    },
+  },
 }
