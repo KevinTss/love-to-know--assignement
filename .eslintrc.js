96562d052409ff567eb5805ee7c2c475dc19e899
KevinTss <kevin.tassi@gmail.com>
2020-09-27 18:33:40 +0200

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
   rules: {
     'vue/component-definition-name-casing': ['warn', 'kebab-case'],
     'vue/max-attributes-per-line': "off",
-    'vue/html-self-closing': "off"
+    'vue/html-self-closing': "off",
+    // "vue/singleline-html-element-content-newline": ["warn", {
+    //   "ignoreWhenNoAttributes": true,
+    //   "ignoreWhenEmpty": true,
+    //   "ignores": ["pre", "textarea", ...INLINE_ELEMENTS]
+    // }]
   },
 }
